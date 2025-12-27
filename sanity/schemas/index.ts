d1922e210fca1abe3b0f15d878c087aee85aba7a
Sam Hammond <sam.hammond1987@gmail.com>
2025-12-27 11:48:41 +1100

    refactor: remove legacy /sanity directory (use jess-sits instead)

--- sanity/schemas/index.ts
+++
@@ -1,7 +0,0 @@
-import testimonial from './testimonial';
-import service from './service';
-import aboutContent from './aboutContent';
-import siteSettings from './siteSettings';
-
-export const schemaTypes = [testimonial, service, aboutContent, siteSettings];
-
